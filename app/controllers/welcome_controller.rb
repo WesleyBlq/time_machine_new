2acc70833d34434b8af4811a2d557123db0fb766
mac bao <254284297@qq.com>
2018-07-18 11:18:30 +0800

    update domain

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,12 +4,12 @@ class WelcomeController < ApplicationController
 	def index
 		# OAuth 2
 		
-		# unless params[:code].present?
-		# 	redirect_to wechat_gate_config.oauth2_entrance_url(scope: "snsapi_userinfo", state: "CURENT_STATE")  
-		# else
-		# 	valid_info = wechat_gate_config.oauth2_access_token(params[:code])
-		# 	@user_info = wechat_gate_config.oauth2_user(valid_info["access_token"], valid_info["openid"])
-		# end
+		unless params[:code].present?
+			redirect_to wechat_gate_config.oauth2_entrance_url(scope: "snsapi_userinfo", state: "CURENT_STATE")  
+		else
+			valid_info = wechat_gate_config.oauth2_access_token(params[:code])
+			@user_info = wechat_gate_config.oauth2_user(valid_info["access_token"], valid_info["openid"])
+		end
 
 	end
 end
